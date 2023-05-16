376bd860eea331e8ef7ca17f967733f5a7a03797
Justinrds2001 <justinrodrigues@live.nl>
2023-05-16 15:27:45 +0200

    Final commit

--- DigitRecognition/main.py
+++ DigitRecognition/main.py
@@ -5,37 +5,54 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Load the MNIST dataset
-(x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+(train_images, train_labels), (
+    test_images,
+    test_labels,
+) = keras.datasets.mnist.load_data()
 
-x_train = x_train.astype('float32') / 255
-x_test = x_test.astype('float32') / 255
+train_images = train_images.astype("float32") / 255
+test_images = test_images.astype("float32") / 255
 
-x_train = np.expand_dims(x_train, axis=-1)
-x_test = np.expand_dims(x_test, axis=-1)
+train_images = np.expand_dims(train_images, axis=-1)
+test_images = np.expand_dims(test_images, axis=-1)
 
-y_train = keras.utils.to_categorical(y_train)
-y_test = keras.utils.to_categorical(y_test)
+train_labels = keras.utils.to_categorical(train_labels)
+test_labels = keras.utils.to_categorical(test_labels)
 
-model = keras.Sequential([
-    keras.layers.Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(28, 28, 1)),
-    keras.layers.MaxPooling2D(pool_size=(2, 2)),
-    keras.layers.Flatten(), 
-    keras.layers.Dense(128, activation='relu'),
-    keras.layers.Dense(10, activation='softmax')
-])
+model = keras.Sequential(
+    [
+        keras.layers.Conv2D(
+            32,
+            kernel_size=(3, 3),
+            activation="relu",
+            input_shape=(28, 28, 1),
+        ),
+        keras.layers.MaxPooling2D(pool_size=(4, 4)),
+        keras.layers.Flatten(),
+        keras.layers.Dense(256, activation="relu"),
+        keras.layers.Dense(128, activation="relu"),
+        keras.layers.Dense(64, activation="relu"),
+        keras.layers.Dense(32, activation="relu"),
+        keras.layers.Dense(10, activation="softmax"),
+    ]
+)
 
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=[tf.keras.metrics.AUC()])
+model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
 
-history = model.fit(x_train, y_train, epochs=3, batch_size=64, validation_data=(x_test, y_test))
+history = model.fit(
+    train_images,
+    train_labels,
+    epochs=4,
+    batch_size=64,
+    validation_data=(test_images, test_labels),
+)
 
-test_loss, test_auc = model.evaluate(x_test, y_test)
+test_loss, test_auc = model.evaluate(test_images, test_labels)
 
-plt.plot(history.history['auc'])
-plt.plot(history.history['val_auc'])
-plt.title('Model AUC')
-plt.ylabel('AUC')
-plt.xlabel('Epoch')
-plt.legend(['Train', 'Test'], loc='upper left')
+plt.plot(history.history["accuracy"])
+plt.plot(history.history["val_accuracy"])
+plt.title("Model Accuracy")
+plt.ylabel("Accuracy")
+plt.xlabel("Epoch")
+plt.legend(["Train", "Test"], loc="upper left")
 plt.show()
-
-model.save("MNIST_2by2poolsize_5epochs.h5")
